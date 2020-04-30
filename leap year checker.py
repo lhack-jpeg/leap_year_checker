8cb0e8a55130533b5bb7e1d43346dde77dd83a01
lhack-jpeg <62835319+lhack-jpeg@users.noreply.github.com>
2020-04-30 14:07:50 +1000

    Add files via upload

---
+++ leap year checker.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 14:04:27 2020
+
+@author: luke-
+"""
+
+year = int(input('Enter a year:'))
+if year%4 == 0:
+  print( 'that is a leap year')
+else:
+  print('that is not a leap year')
